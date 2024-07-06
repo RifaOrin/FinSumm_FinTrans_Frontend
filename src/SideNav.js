c0eb67f65f2a9d1f785ec6c5edbee95ca2e192a3
RifaOrin <rifaorin1234@gmail.com>
2024-07-06 21:28:04 +0600

    Home page design added

--- src/SideNav.js
+++ src/SideNav.js
@@ -9,7 +9,7 @@ const SideNav = () => {
 
     return(
             
-        <div className={`bg-dark-white h-screen z-200 p-5 pt-8 ${open ? "w-80" : "w-20"} duration-300 sticky top-0`}>
+        <div className={`bg-dark-white h-screen z-200 p-5 pt-8 ${open ? "w-72" : "w-20"} duration-300 sticky top-0`}>
         <BsChevronDoubleLeft className={`text-black text-xl font-bold absolute cursor-pointer top-9 ${
             open ? "right-6" : "left-1/2 transform -translate-x-1/2 rotate-180"}`} onClick={() => setOpen(!open)} />
         <div className={`rounded-full mt-20 border bg-max-dark-white items-center p-2 transition-all duration-300 ${open ? "inline-flex" : "flex justify-center"}`}>
